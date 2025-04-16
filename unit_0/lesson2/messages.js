2a91ead24b6d92dac2107fcf69402c8c6c83df03
marvek16 <marvelousk1603f@gmail.com>
2025-04-16 10:13:06 +0200

    first commit

---
+++ unit_0/lesson2/messages.js
@@ -0,0 +1,5 @@
+"use strict";
+
+let messages= ["Marvelous", 19, true];
+
+messages.forEach(message => console.log(message));
---
+++ unit_1/lesson3/module_3_1/messages.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// exports.messages = [
+//   "You are great!",
+//   "You can accomplish anything!",
+//   "Success is in your future!"
+// ];
+
+exports.messages = ["Good Job!", "Welldone.", "Have a great day."];
