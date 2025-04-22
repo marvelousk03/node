d17217e7e7142d1a9b109784c937ae47c427c064
marvek16 <marvelousk1603f@gmail.com>
2025-04-22 11:11:23 +0200

    commit

--- unit_1/lesson6/serve_html/main.js
+++ unit_1/lesson6/serve_html/main.js
@@ -1,29 +1,40 @@
-"use strict";
+const port = 3000;
+const http = require("http");
+const httpStatusCodes = require("http-status-codes");
+const router = require("./router");
+const fs = require("fs");
 
-const port = 3000,
-    http = require("http"),
-    httpStatus = require("http-status-codes"),
-    fs = require("fs");
-const getViewUrl = url => {
-    return `views${url}.html`;
+const plainTextContentType = {
+    "Content-Type": "text/plain"
 };
 
-http
-    .createServer((req, res) => {
-        let viewUrl = getViewUrl(req.url);
-        fs.readFile(viewUrl, (error, data) => {
-            if (error) {
-                res.writeHead(httpStatus.NOT_FOUND);
-                res.write("<h1>FILE NOT FOUND</h1>");
-            } else {
-                res.writeHead(httpStatus.OK, {
-                    "Content-Type": "text/html"
-                });
-                res.write(data);
-            }
-            res.end();
-        });
-    })
+const htmlContentType = {
+    "Content-Type": "text/html"
+};
 
-    .listen(port);
-console.log(`The server has started and is listening on port number: ${port}`);
+const customReadFile = (file, res) => {
+    fs.readFile(`./${file}`, (errors, data) => {
+        if (errors) {
+            console.log("Error reading the file...");
+        }
+        res.end(data);
+    });
+};
+
+router.get("/", (req, res) => {
+    res.writeHead(httpStatusCodes.OK, plainTextContentType);
+    res.end("INDEX");//plain text
+});
+
+router.get("/index.html", (req, res) => {
+    res.writeHead(httpStatusCodes.OK, htmlContentType);
+    customReadFile("views/index.html", res);//index.html
+});
+
+router.post("/", (req, res) => {
+    res.writeHead(httpStatusCodes.OK, plainTextContentType);
+    res.end("POSTED");//plain text
+});
+
+http.createServer(router.handle).listen(3000);
+console.log(`The server is listening on port number: ${port}`);
